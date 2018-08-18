a361f288c8c95bea2f36f1385b3a17592f7a8b6f
Rohit Maharjan <Ro@192-168-1-28.tpgi.com.au>
2018-08-18 21:34:44 +1000

    Created skeleton of the project and added UI module with header, layout and footer component.

---
+++ subject-review/src/app/login/login.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoggingComponent } from './logging/logging.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LoggingComponent]
+})
+export class LoginModule { }
