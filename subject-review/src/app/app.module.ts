bff74f736a68d49e49fb8f01a428c661a20273a4
Dalley <senghuotlay86@gmail.com>
2018-08-22 16:33:53 +1000

    Added Registration Component

--- subject-review/src/app/app.module.ts
+++ subject-review/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
 import { LoginModule } from './login/login.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
