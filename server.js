71a2d83981657241c6baf60789e1896dbf6cb515
Dalley <senghuotlay86@gmail.com>
2018-08-25 13:14:09 +1000

    set up base platform for users and registration

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-var express = require('express')
-var path = require('path')
-var bodyParser = require('body-parser')
-var cors = require('cors')
-var passport = require('passport')
-var mongoose = require('mongoose')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const passport = require('passport')
+const mongoose = require('mongoose')
 
-var app = express()
+const app = express();
 
+const users = require('./routes/users');
+
+//port number
 const port = 3000;
 
-app.get('/', () =>{
+app.use(cors());
+
+//Body parse that allows forms to be accepted as data
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//index route
+app.get('/', () => {
     res.send('hello world');
 })
 
+//start server
 app.listen(port, () => {
-    console.log('server is listening on port ' + port)
-})
+    console.log('server is listening on port ' + port);
+});
