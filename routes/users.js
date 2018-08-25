dd19326cf2457c31ec8db6030a4d52208cf4e3d5
Dalley <senghuotlay86@gmail.com>
2018-08-25 13:55:50 +1000

    Connected the server to the database, and preparing for database setup

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 //Register
 router.get('/register', (req,res,next) => {
-    res.send('register')
+    res.send('Profile')
+});
+
+//Authenticate
+router.post('/authenticate', (req,res,next) => {
+    res.send('Authenticate')
+});
+
+//Profile
+router.get('/profile', (req,res,next) => {
+    res.send('Profile')
+});
+
+//Validate
+router.get('/validate', (req,res,next) => {
+    res.send('Validate')
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const passport = require('passport')
 const mongoose = require('mongoose')
+const config = require('./config/database')
+
+//connection to the database
+mongoose.connect(config.database, {useNewUrlParser: true});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to the database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database ' + err);
+});
 
 const app = express();
 
@@ -12,7 +24,11 @@ const users = require('./routes/users');
 //port number
 const port = 3000;
 
+//Cors middleware
 app.use(cors());
+
+//Connect the server to angular file
+app.use(express.static(path.join(__dirname,'subject-review')))
 
 //Body parse that allows forms to be accepted as data
 app.use(bodyParser.json());
