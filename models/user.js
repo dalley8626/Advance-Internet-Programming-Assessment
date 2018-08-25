9f188efe6b91728b3d1cb0abe0d18bb74d8d7512
Dalley <senghuotlay86@gmail.com>
2018-08-25 16:01:27 +1000

    Basic registration functionality is working by using postman to send the message

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//User schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type:String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = (id, callback) => {
+    User.findById(id,callback);
+}
+
+module.exports.getUserByEmailAddress = (email,callback) => {
+    const query = {email: email}
+    User.findOne(query,callback);
+}
+
+module.exports.addUser = (newUser, callback) => {
+    bcrypt.genSalt(10, (err,salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    })
+}
