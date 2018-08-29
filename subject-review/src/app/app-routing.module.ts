d05940a0c8d3d64b96858c7b620ae8235b22c952
Rohit Maharjan <Ro@192-168-1-28.tpgi.com.au>
2018-08-29 18:59:50 +1000

    Added User module for user display, components from subject are not yet imported to display subjects

--- subject-review/src/app/app-routing.module.ts
+++ subject-review/src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisteringComponent } from './register/registering/registering.component';
-import { LoggingComponent } from './login/logging/logging.component'
-import { DashboardComponent } from './subject/dashboard/dashboard.component'
-import { SubjectsComponent } from './subject/subjects/subjects.component'
+import { LoggingComponent } from './login/logging/logging.component';
+import { DashboardComponent } from './subject/dashboard/dashboard.component';
+import { SubjectsComponent } from './subject/subjects/subjects.component';
+import { SubjectDetailComponent } from './subject/subject-detail/subject-detail.component';
+import { UsersComponent } from './user/users/users.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoggingComponent },
   { path: 'register', component: RegisteringComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'subject', component: SubjectsComponent }
-  
-
+  { path: 'subject', component: SubjectsComponent },
+  { path: 'detail/:id', component: SubjectDetailComponent },
+  { path: 'users', component: UsersComponent }
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
